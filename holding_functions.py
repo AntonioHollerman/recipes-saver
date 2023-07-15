2cc7dbaaeef22b059ce24c8af7321ade79a5bb92
Antonio Hollerman <toniohollerprof@gmail.com>
2023-07-14 20:02:53 -0400

    Updated database to have test information. Created the needed functions for operating the sqlite database.

--- holding_functions.py
+++ holding_functions.py
@@ -1,4 +1,6 @@
 import sqlite3
+import os
+import webbrowser
 
 db_conn = sqlite3.connect('recipes_info.sqlite', detect_types=sqlite3.PARSE_DECLTYPES)
 db_cur = db_conn.cursor()
@@ -30,11 +32,12 @@ def add_recipe(name, ingredients: list | tuple, image_path, desc, instructions, 
     :param name: The name of the recipe
     :param ingredients: The ingredients for the recipe
     :param image_path: File path to the image
-    :param desc:
-    :param instructions:
-    :param instruction_type:
+    :param desc: The description of the meal
+    :param instructions: The web link or file for the instructions of the recipe
+    :param instruction_type: Informs if the instructions are a web link or a file
     :return: None
     """
+    ingredients = "-(.o)0)0_-23".join(ingredients)
     global next_recipe_id
     db_cur.execute("INSERT INTO recipes "
                    "(recipe_id, recipe_name, recipe_ingredients, recipe_image, recipe_desc, "
@@ -45,6 +48,64 @@ def add_recipe(name, ingredients: list | tuple, image_path, desc, instructions, 
     next_recipe_id += 1
 
 
-def remove_recipe(recipe_id):
+def remove_recipe(recipe_id: int):
+    """
+    Removes a recipe from the database
+    :param recipe_id: The id for the recipe that wants to be deleted
+    :return: None
+    """
     db_cur.execute("DELETE FROM recipes "
                    f"WHERE recipe_id = {recipe_id}")
+
+
+def open_instructions(recipe_id: int):
+    """
+    Execute a file or open the browser of the wanted recipe
+    :param recipe_id: The id for the recipe that wants to be deleted
+    :return: None
+    """
+    db_cur.execute(f"SELECT recipe_instructions, instructions_type FROM recipes WHERE recipe_id = {recipe_id}")
+    recipe_instructions, instructions_type = db_cur.fetchone()
+    if instructions_type == "web_link":
+        webbrowser.open(recipe_instructions)
+        return True
+    elif instructions_type == "file_location":
+        found_path = os.path.exists(recipe_instructions)
+        if found_path:
+            os.system(f"start {recipe_instructions}")
+            return True
+        else:
+            return False
+    else:
+        return False
+
+
+def all_saved_recipes():
+    """
+    Returns every row
+    :return: A list of rows that is a tuple that contains (recipe_id, recipe_name, recipe_ingredients, recipe_image,
+    recipe_desc, recipe_instructions, instructions_type)
+    """
+    db_cur.execute("SELECT recipe_id, recipe_name, recipe_ingredients, recipe_image, recipe_desc, recipe_instructions, "
+                   "instructions_type FROM recipes ORDER BY recipe_id")
+    data = db_cur.fetchall()
+    to_return = []
+    for row in data:
+        recipe_id, recipe_name, recipe_ingredients, recipe_image, recipe_desc, recipe_instructions, \
+            instructions_type = row
+        recipe_ingredients = recipe_ingredients.join("-(.o)0)0_-23")
+        to_return.append((recipe_id, recipe_name, recipe_ingredients, recipe_image, recipe_desc, recipe_instructions,
+                         instructions_type))
+    return to_return
+
+
+def get_recipe(recipe_id: int):
+    """
+    Returns one row
+    :param recipe_id: The id of the recipe wanted
+    :return: The row is a tuple that contains (recipe_id, recipe_name, recipe_ingredients, recipe_image, recipe_desc,
+    recipe_instructions, instructions_type)
+    """
+    db_cur.execute("SELECT recipe_id, recipe_name, recipe_ingredients, recipe_image, recipe_desc, recipe_instructions, "
+                   f"instructions_type FROM recipes WHERE recipe_id = {recipe_id}")
+    return db_cur.fetchone()
