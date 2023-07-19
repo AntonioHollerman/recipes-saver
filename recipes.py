de9564e5e4c0e7916fa79131155350c0ad7040c7
Antonio Hollerman <toniohollerprof@gmail.com>
2023-07-19 00:35:20 -0400

    Retrieve information from saved recipes and commit the changes to the recipes_info.sqlite database.

--- recipes.py
+++ recipes.py
@@ -1,10 +1,21 @@
 from window_classes import *
 
 main_window = RecipesWindow()
-main_window.run()
+main_window.mainloop()
 
 if main_window.current_window == 'Edit Frame':
-    main_window.edit_frame.save_recipe()
+    main_window.current_window.save_recipe()
+
+all_ids = get_ids()
+for recipe_id, recipe in main_window.recipes.items():
+    if recipe_id in all_ids:
+        update_recipe(recipe)
+    else:
+        add_recipe(*recipe[1:])
+
+for recipe_id in all_ids:
+    if recipe_id not in main_window.recipes:
+        remove_recipe(recipe_id)
 
 db_conn.commit()
 db_cur.close()
